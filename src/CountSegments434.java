0a3687bd9f2d0b6ba21b46ddaa0b09cc7a150327
wangzepeng <stoneepigraph@163.com>
2021-10-07 22:46:13 +0800

    ContainsDuplicate217

--- src/CountSegments434.java
+++ src/CountSegments434.java
@@ -1,4 +1,9 @@
 public class CountSegments434 {
+    /**
+     * 设置临时字符为空，遍历字符串，如果当前字符不为空并且上一个字符为空，总数加一，每一次遍历都反当前字符赋值给临时字符。
+     * @param s
+     * @return
+     */
     public static int countSegments(String s) {
         if (s == null) {
             return 0;
